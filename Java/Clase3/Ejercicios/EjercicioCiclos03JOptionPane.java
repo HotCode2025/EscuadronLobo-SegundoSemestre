53fbc51e74881572a655c9940b427c46d73fb62b
Lucas Nicolas Aguilar <Lucas7u7nicolas@gmail.com>
2025-08-27 20:41:08 -0300

    Agrego Ejercicio Ciclos03 Scanner y JOptionPane

---
+++ Java/Clase3/Ejercicios/EjercicioCiclos03JOptionPane.java
@@ -0,0 +1,22 @@
+
+package ejerciciociclos03;
+
+import javax.swing.JOptionPane;
+
+public class EjercicioCiclos03JOptionPane {
+    public static void main(String[] args) {
+        int numero;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número (0 para salir): "));
+        while(numero != 0){
+            if(numero % 2 == 0){
+                JOptionPane.showMessageDialog(null, "El numero " + numero + " es par");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "El número " + numero + " es impar");             
+            }
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro número (0 para terminar): "));
+        }
+        JOptionPane.showMessageDialog(null,"Finalizaste el programa con " + numero);
+    }
+}
