5bae7f9c6bfaed64e044b5c37dcee0ed2777eb29
Cecilia Farias <Cecyaf25@gmail.com>
2025-09-01 22:21:12 -0300

    Actualiso y borro ejercicio repetido

--- Python/Clase4/5.1_Ejercicio 4 Suma dentro de un rango.py
+++ Python/Clase4/5.1_Ejercicio 4 Suma dentro de un rango.py
@@ -1,3 +1,10 @@
+#Ejercicio 4: Sumar números pares dentro de un rango
+#Hacer un programa para sumar números pares dentro de un rango,
+#por ejemplo: 
+#       suma de numeros pares del 2 al 30
+#       suma = 240
+
+
 inicio = int(input("Ingrese el número inicial del rango: "))
 fin = int(input("Ingrese el número final del rango: "))
 suma_pares = 0
