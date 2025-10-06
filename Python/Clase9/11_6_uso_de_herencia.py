254f63ae5b7c38a8d2af417524c3ef31a8217158
Juan Moreno <juan.moreno1994e@gmail.com>
2025-10-06 20:00:29 -0300

    Agrego el enunciado de ejercicio 11.6 python

---
+++ Python/Clase9/11_6_uso_de_herencia.py
@@ -0,0 +1,16 @@
+class Vehiculo:
+    '''
+    Definir una clase padre llamada Vehiculo y dos clases hijas llamadas
+    Auto y Bicicleta, las cuales heredan de la clase padre Vehiculo. La clase
+    padre debe tener los siguientes atributos y métodos:
+    Vehiculo (clase padre)
+    -Atributos(color, ruedas)
+    -Métodos (____init__(color, ruedas) y ___str__())
+    Auto(clase hija de Vehiculo)
+    -Atributos (velocidad (km/hr))
+    -Métodos(____init__(color, ruedas, velocidad) y ___str___())
+    Bicicleta (clase hija de Vehiculo)
+    -Atributos (tipo(urbana/montaña/etc.)
+    -Métodos(____init__(color, ruedas, tipo) y ___str__()
+    Crear un objeto de cada clase
+    '''
