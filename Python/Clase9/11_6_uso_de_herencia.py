796acd3087c8a4a566b7723ba3c5df28a8de2b5b
Juan Moreno <juan.moreno1994e@gmail.com>
2025-10-06 21:33:38 -0300

    Agrego tarea ejercicio 11.6 clase 9

--- Python/Clase9/11_6_uso_de_herencia.py
+++ Python/Clase9/11_6_uso_de_herencia.py
@@ -14,3 +14,42 @@ class Vehiculo:
     -Métodos(____init__(color, ruedas, tipo) y ___str__()
     Crear un objeto de cada clase
     '''
+
+
+class Vehiculo:
+    
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return f"Color: {self.color}, Ruedas: {self.ruedas}"
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return f"Auto -> {super().__str__()}, Velocidad: {self.velocidad} km/h"
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return f"Bicicleta -> {super().__str__()}, Tipo: {self.tipo}"
+
+
+# Crear un objeto de cada clase
+vehiculo1 = Vehiculo("Rojo", 4)
+auto1 = Auto("Azul", 4, 180)
+bicicleta1 = Bicicleta("Verde", 2, "Montaña")
+
+# Mostrar los objetos
+print(vehiculo1)
+print(auto1)
+print(bicicleta1)
