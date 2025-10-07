457f8b7d7670af498e6ad0fba4a6f9126f283724
Matias <126737037+mathias5525@users.noreply.github.com>
2025-10-06 22:46:04 -0300

    11.5 Método dunder __str__()

---
+++ Python/Clase9/11-5_Metodo_dunder__str__.py
@@ -0,0 +1,8 @@
+# Python/Clase9/11-5_Metodo_dunder__str__.py
+
+from Herencia_la_teoria_en_UML import * # type: ignore
+persona1 = Persona("Osvaldo", 40)
+print(persona1)
+
+empleado3 = Empleado('Pablo', 23, 57000)
+print(empleado3)
