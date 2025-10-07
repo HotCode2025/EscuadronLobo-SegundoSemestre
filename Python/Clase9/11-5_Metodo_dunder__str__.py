23fd518a11c89d5cdbfd22d115f367f0b75ef41e
Matias <126737037+mathias5525@users.noreply.github.com>
2025-10-06 22:46:49 -0300

    Update 11-5_Metodo_dunder__str__.py

--- Python/Clase9/11-5_Metodo_dunder__str__.py
+++ Python/Clase9/11-5_Metodo_dunder__str__.py
@@ -1,8 +1,9 @@
 # Python/Clase9/11-5_Metodo_dunder__str__.py
 
-from Herencia_la_teoria_en_UML import * # type: ignore
+from Herencia_la_teoria_en_UML import * 
 persona1 = Persona("Osvaldo", 40)
 print(persona1)
 
 empleado3 = Empleado('Pablo', 23, 57000)
+
 print(empleado3)
