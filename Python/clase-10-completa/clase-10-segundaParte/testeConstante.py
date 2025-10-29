99d26aa9b1f709c8f3090cb24eb196789155eb1c
Dario Scilipoti <dariosc@gmail.com>
2025-10-29 09:36:41 -0300

    Upload new Clase 10 - Python

--- Python/clase-10-completa/clase-10-segundaParte/testeConstante.py
+++
@@ -1,4 +0,0 @@
-from constantes import *
-from  constantes import MI_CONSTANTE as MC #agregarle el as le da como un alias para las importaciones con nombres largos
-print(MI_CONSTANTE)
-print(MC)
--- Python/clase-10-completa/clase-10/validaciones/aqui/rectangulo.py
+++
@@ -1,9 +0,0 @@
-from Figurageometrica import *
-from Color import  *
-class rectangulo(Figurageometrica, Color):
-    def __init__(self, alto, ancho, color):
-        Figurageometrica.__init__(self, alto, ancho)
-        Color.__init__(self, color)
-
-    def area(self):
-        return self._alto * self._ancho
