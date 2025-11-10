ecc4646f40bbfbc1cccb062979bc6a46bebff285
Matias <126737037+mathias5525@users.noreply.github.com>
2025-11-10 11:02:45 -0300

    update ejercicio 4 clase3_JOpcionPane

--- Java/Clase3/Ejercicios/Ejercicio3_2_JOpcionPane.java
+++ Java/Clase3/Ejercicios/Ejercicio3_2_JOpcionPane.java
@@ -2,6 +2,30 @@
 //Ejercicio 4: Pedir numeros hasta que se teclee uno negativo, y mostrar cuantos numeros
 //se han introducido. Clase JOpcionPane
 
-public class Ejercicio3_2_JOpcionPane {
-    
+import javax.swing.JOptionPane;
+
+public class Ejercicio4JOptionPane {
+    public static void main(String[] args) {
+
+        int numero;
+        int contador = 0;
+
+        do {
+            String input = JOptionPane.showInputDialog(
+                "Ingrese un número (negativo para salir):"
+            );
+
+            numero = Integer.parseInt(input);
+
+            if (numero >= 0) {
+                contador++;
+            }
+
+        } while (numero >= 0);
+
+        JOptionPane.showMessageDialog(
+            null, 
+            "Cantidad de números ingresados: " + contador
+        );
+    }
 }
