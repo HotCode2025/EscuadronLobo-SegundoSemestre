8f2ba5f56035dd8cdbf7cce8350d4e257688dad7
Matias <126737037+mathias5525@users.noreply.github.com>
2025-11-10 22:12:41 -0300

    Update Usuario Cajero

--- proyecto_restaurante/src/domain/usuarios/Cajero.java
+++ proyecto_restaurante/src/domain/usuarios/Cajero.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domain.usuarios;
 
-/**
- *
- * @author MATIAS-INFORMATICA
- */
-public class Cajero {
-    
+public class Cajero extends Empleado {
+
+    public Cajero(String nombre, String dni, String usuario, String contrasena, String sucursal) {
+        super(nombre, dni, usuario, contrasena, sucursal, "Cajero");
+    }
+
+    public void cobrarPedido(double monto) {
+        System.out.println(getNombre() + " cobró $" + monto + " del cliente.");
+    }
+
+    public void generarTicket(String pedido) {
+        System.out.println("El cajero " + getNombre() + " generó el ticket para el pedido: " + pedido);
+    }
+
+    @Override
+    public String toString() {
+        return "Cajero{" +
+                "nombre='" + getNombre() + '\'' +
+                ", sucursal='" + getSucursal() + '\'' +
+                '}';
+    }
 }
